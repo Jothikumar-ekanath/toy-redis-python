26baf33f19008157d0b4e32b155cbb0963206eb8
jothikumar <jothikumar.kuppaekanath@nike.com>
2024-04-21 23:10:21 -0700

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
-# Uncomment this to pass the first stage
 import socket
+
+
+def handle_connection(client_socket: socket) -> None:
+    with client_socket:
+        # _ = client_socket.recv(1024)
+        # client_socket.sendall("+PONG\r\n".encode())
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            client_socket.sendall("+PONG\r\n".encode())
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, _addr = server_socket.accept() # wait for client
-    client_socket.sendall("+PONG\r\n".encode("utf-8"))
-    client_socket.close()
+    handle_connection(client_socket)
 
 
 if __name__ == "__main__":
