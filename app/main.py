b220fe339a578232501bc4897a27c07474a61719
jothikumar <jothikumar.kuppaekanath@nike.com>
2024-04-22 10:44:34 -0700

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
 import socket
+import asyncio
 
+async def connection_handler(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+):
+    try:
+        addr = writer.get_extra_info("peername")
+        while True:
+            request = await reader.readline()
+            if request == b"":
+                break
+            elif b"ping" in request:
+                writer.write(b"+PONG\r\n")
+            print(f"sent response to data: {request}, for {addr}")
+        await writer.drain()
+    except Exception as e:
+        print(f"An error occurred for {addr}: {e}")
+    finally:
+        writer.close()
 
+# Simple connection handler using socket
 def handle_connection(client_socket: socket) -> None:
     with client_socket:
         # _ = client_socket.recv(1024)
@@ -12,13 +31,21 @@ def handle_connection(client_socket: socket) -> None:
             client_socket.sendall("+PONG\r\n".encode())
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _addr = server_socket.accept() # wait for client
-    handle_connection(client_socket)
+async def main():
+    # print("Logs from your program will appear here!")
+    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # client_socket, _addr = server_socket.accept() # wait for client
+    # handle_connection(client_socket)
+    server = await asyncio.start_server(connection_handler, "localhost", 6379)
+    print(f"Server running on {server.sockets[0].getsockname()}")
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
